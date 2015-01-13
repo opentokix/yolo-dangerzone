714121035b439d2ad4e1b7669e2fece74e71d9d0
Peter Eriksson <peter@sysadmin.wigetmedia.com>
2015-01-13 12:56:42 +0000

    Added add_dmesg, util to add a manual timestamp to your dmesg kernel log

---
+++ add_dmesg/.tmp_versions/add_mark_to_dmesg.mod
@@ -0,0 +1,2 @@
+/home/peter/src/add_dmesg/add_mark_to_dmesg.ko
+/home/peter/src/add_dmesg/add_mark_to_dmesg.o
---
+++ add_dmesg/add_mark_to_dmesg.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+int init_module(void)
+{
+    struct timeval t;
+    struct tm broken;
+    do_gettimeofday(&t);
+    time_to_tm(t.tv_sec, 0, &broken);
+    
+    printk(KERN_INFO "-- MARK -- Added manually at: %ld-%02d-%02d %02d:%02d:%02d\n", 1900 + broken.tm_year, 
+                                                                          broken.tm_mon + 1, 
+                                                                          broken.tm_yday, 
+                                                                          broken.tm_hour, 
+                                                                          broken.tm_min, 
+                                                                          broken.tm_sec);
+
+    
+    return 0;
+}
+
+void cleanup_module(void)
+    {
+    }
