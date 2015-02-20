24247d93b8cba1d1f26f2feecedbe0975fe3e72e
OpenTokix <petereriksson.net@gmail.com>
2015-02-20 16:57:21 +0100

    Tidying up the code, adding comments and inserting db table create command

--- mysql-tools/populate_db.py
+++ mysql-tools/populate_db.py
@@ -42,33 +42,50 @@ def uniquePhrases(no_phrases):
             yield this_phrase
 
 def r_string(length):
+	"""
+	Generate a random string of <length>
+	"""
 	lst = [random.choice(string.ascii_letters) for n in xrange(length)]
 	return "".join(lst)
 
 def generate_id():
+	"""
+	Get a very long int based on tiem since epoch
+	"""
 	# This will take time in nanoseconds, and present it as a large int
 	return int(time.time()*1000000)
 
 def main():
+	"""
+	This is the schema used for the test table in test database
+	CREATE TABLE test (
+	                   id BIGINT NOT NULL AUTO_INCREMENT,
+	                   data INT,
+	                   data2 VARCHAR(20)
+	                   PRIMARY KEY (id)
+	                   ) CHARSET=utf8;
+	"""
+
+	# Creating the database connection
 	connection = MySQLdb.connect (host = "127.0.0.1", user = "test", passwd = "test", db = "test")
 
+	num = 100 #number of rows to add
 
-	num = 100
-	information = "Populating database with %d rows: " % (num)
+	information = "Populating database with %d rows: " % (num) # some info on the conole
 	sys.stdout.write(information)
 	sys.stdout.flush()
 
 	for i in range(1, num):
-		if i % (num/10) == 0:
+		if i % (num/10) == 0: # Will write a progress bar each 1/10th of the progress
 			progress = "%d.." % (i)
 			sys.stdout.write(progress)
-			sys.stdout.flush()
+			sys.stdout.flush() # Flushing on each iteration, to make sure it goes to console
 
 		cursor = connection.cursor()
-		time_id = generate_id()
-		random_data = r_string(2)
-		random_data2 = phrase(1)
-		for attempt_number in range(3):
+
+		random_data2 = phrase(1) # Getting a word
+
+		for attempt_number in range(3): # Doing x attempts before throwing error
 			try:
 				cursor.execute("INSERT INTO test(data, data2) VALUES ('%d', '%s')" % (random.randint(1,9), random_data2))
 				connection.commit()
@@ -76,7 +93,7 @@ def main():
 			except MySQLdb.Error, e:
 				print "MySQL Error %d: %s" % (e.args[0], e.args[1])
 
-		time.sleep(.5)
+		time.sleep(.5) # Sleeping for half a second before each insert, this is not a benchmark
 
 	cursor.close()
 	connection.close()
