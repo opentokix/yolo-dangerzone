9ef46c876ea58355c22b1fd1298937febf549158
Peter Eriksson <peter.eriksson@wigetmedia.com>
2016-01-18 16:19:01 +0100

    Various script and projects added

---
+++ add_dmesg/.tmp_versions/add_mark_to_dmesg.mod
@@ -0,0 +1,2 @@
+/home/peter/src/yolo-dangerzone/add_dmesg/add_mark_to_dmesg.ko
+/home/peter/src/yolo-dangerzone/add_dmesg/add_mark_to_dmesg.o
---
+++ add_dmesg/add_mark_to_dmesg.mod.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xca05c877, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0x92a9c60c, __VMLINUX_SYMBOL_STR(time_to_tm) },
+	{ 0x4f68e5c9, __VMLINUX_SYMBOL_STR(do_gettimeofday) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "2D3BEF25F55098E11A228A7");
