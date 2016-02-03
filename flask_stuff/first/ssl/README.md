ec96082b4717287daf2972b1f77652aff66dc3c0
Peter Eriksson <peter.eriksson@wigetmedia.com>
2016-02-03 11:56:50 +0100

    Added readme how to create own certs

---
+++ flask_stuff/first/ssl/README.md
@@ -0,0 +1,26 @@
+How to create your own ssl certs 
+
+Install openssl with your package manager 
+
+apt-get install openssl 
+
+
+Generating a server key with pass phrase 
+openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+
+Remove the pass phrase 
+openssl rsa -passin pass:x -in server.pass.key -out server.key
+rm server.pass.key
+
+Generate the cert signing request, based on your key 
+openssl req -new -key server.key -out server.csr
+
+Fill out any information, like your state and such. 
+
+For CN write: * or some FQDN 
+
+Generate the actual cert based on your csr and key 
+
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+
--- ls_logger.py
+++ ls_logger.py
@@ -6,13 +6,20 @@ import logstash
 def main():
     test_logger = logging.getLogger('python-logstash-logger')
     test_logger.setLevel(logging.INFO)
-    test_logger.addHandler(logstash.TCPLogstashHandler(host='172.16.135.1', port=5050, version=1))
+    test_logger.addHandler(logstash.TCPLogstashHandler(host='172.17.0.2', port=5050, version=1))
     test_logger.info('python-logstash: test logstash info message.')
+
+    s_logger = logging.getLogger('python-syslog-logger')
+    s_logger.setLevel(logging.INFO)
+    s_handler = logging.handlers.SysLogHandler(address=('172.17.0.2', 5000),facility=19)
+    s_logger.addHandler(s_handler)
+    s_logger.info('python-syslog: test logstash info message.')
+
     try:
         1/0
     except:
         test_logger.exception('python-logstash-logger: Exception with stack trace!')
-
+        s_logger.exception('python-syslog-loggger: Exception wwwith stack trace!')
 
 
 
