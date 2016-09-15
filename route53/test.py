67c0d4135ab42d8f9ddf8d532ef978ad8ae9927e
Peter Eriksson <peter.eriksson@wigetmedia.com>
2016-09-15 16:33:21 +0200

    Working route53 updater

--- route53/test.py
+++ route53/test.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python2
+"""Tool to handle route53 hosted domains."""
 import configparser
 import boto3
 
+
 def readconfig():
-    conf = { 'access_key': 'undefined', 'secret': 'undefined' }
+    """Read credentials and other optional config options."""
+    conf = {'access_key': 'undefined', 'secret': 'undefined'}
     config = config = configparser.ConfigParser()
     config.read('/home/peter/credentials/aws.cred')
     if 'ROUTE53' in config:
@@ -12,29 +15,36 @@ def readconfig():
     return conf
 
 
-def update_ot(route53, record, ip):
-   response = route53.change_resource_record_sets(
-	HostedZoneId='',
-	ChangeBatch={
-	    'Comment': 'Python test dns record',
-	    'Changes': [
-		{
-		    'Action': 'UPSERT',
-		    'ResourceRecordSet': {
-			'Name': record,
-			'Type': 'A',
-			'TTL': 300,
-			'ResourceRecords': [
-			    {
-				'Value': ip
-			    },
-			],
-		    }
-		},
-	    ]
-	}
-    )
+def get_available_zones(route53):
+    """Get zones handled by this account, for some basic checks."""
+    response = route53.list_hosted_zones()
+    return response
 
+
+def get_zone_id(route53, domain):
+    """Return the hostid for a domainname."""
+    # return jsondata['HostedZones'][]
+    response = route53.list_hosted_zones()
+    search_domain = domain + "."
+    for i in range(len(response['HostedZones'])):
+        if response['HostedZones'][i]['Name'] == search_domain:
+            return response['HostedZones'][i]['Id']
+
+
+def update_ot(route53, domain, record, ip):
+    """Update record in route 53 hosted zone."""
+    full_record = record + '.' + domain + '.'
+    response = route53.change_resource_record_sets(
+        HostedZoneId=get_zone_id(route53, domain), ChangeBatch={'Comment': 'Python test dns record', 'Changes': [
+            {'Action': 'UPSERT', 'ResourceRecordSet': {
+                'Name': full_record, 'Type': 'A', 'TTL': 300, 'ResourceRecords':
+                [
+                    {'Value': ip},
+                ],
+            }
+            },
+        ]})
+    return response
 
 
 def main():
@@ -43,10 +53,9 @@ def main():
     route53 = boto3.client('route53',
                            aws_access_key_id=conf['access_key'],
                            aws_secret_access_key=conf['secret'])
-    response = update_ot(route53, 'bar.opentokix.com', '185.35.77.26')
+    response = update_ot(route53, 'opentokix.com', 'foobar', '185.35.77.26')
     print response
+
 
 if __name__ == '__main__':
     main()
-
-
