502936d3cac17a1adf9701dee5a9665b62becf40
Peter Eriksson <petereriksson.net@gmail.com>
2016-11-22 14:41:58 +0100

    Working

--- route53/dyn_route53.py
+++ route53/dyn_route53.py
@@ -7,11 +7,18 @@ import boto3
 def readconfig():
     """Read credentials and other optional config options."""
     conf = {'access_key': 'undefined', 'secret': 'undefined'}
-    config = config = configparser.ConfigParser()
+    config = configparser.ConfigParser()
     config.read('/home/peter/credentials/aws.cred')
     if 'ROUTE53' in config:
         conf['access_key'] = config['ROUTE53']['access_key']
         conf['secret'] = config['ROUTE53']['secret_access_key']
+    try:
+        config.read('/home/peter/credentials/route53_dyn.conf')
+        for k, v in config:
+            print "key: %s value: %s" & (k, v)
+    except:
+        continue
+
     return conf
 
 
@@ -50,11 +57,11 @@ def update_ot(route53, domain, record, ip):
 def main():
     """Main function."""
     conf = readconfig()
-    route53 = boto3.client('route53',
+    #route53 = boto3.client('route53',
                            aws_access_key_id=conf['access_key'],
                            aws_secret_access_key=conf['secret'])
-    response = update_ot(route53, 'meodo.com', 'foo', '185.35.77.26')
-    print response
+    #response = update_ot(route53, 'meodo.com', 'foo', '185.35.77.26')
+    print conf
 
 
 
