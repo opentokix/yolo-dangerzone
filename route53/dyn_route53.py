96a746689c2fc76d7578171d5e2c1c2a9aa94b90
Peter Eriksson <peter.eriksson@wigetmedia.com>
2016-12-14 17:10:23 +0100

    Added fucntion to get ip

--- route53/dyn_route53.py
+++ route53/dyn_route53.py
@@ -4,6 +4,9 @@ import configparser
 import boto3
 from subprocess import check_output
 import sys
+from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
+import netifaces as ni
+
 
 def readconfig():
     """Read credentials and other optional config options."""
@@ -33,6 +36,15 @@ def readconfig():
     return conf, domain
 
 
+def get_if_addr(interface="eth0", version="ipv4"):
+    """Returning the address of the interface, defaults to ipv4."""
+    if version == 'ipv6':
+        v = "AF_INET6"
+    else:
+        v = "AF_INET"
+    return ni.ifaddresses(interface)[v][0]['addr']
+
+
 def get_available_zones(route53):
     """Get zones handled by this account, for some basic checks."""
     response = route53.list_hosted_zones()
@@ -58,7 +70,7 @@ def update_ot(route53, domain, record, ip, version=4):
 
     full_record = record + '.' + domain + '.'
     response = route53.change_resource_record_sets(
-        HostedZoneId=get_zone_id(route53, domain), ChangeBatch={'Comment': 'Python test dns record', 'Changes': [
+        HostedZoneId=get_zone_id(route53, domain), ChangeBatch={'Comment': 'Autoupdated record', 'Changes': [
             {'Action': 'UPSERT', 'ResourceRecordSet': {
                 'Name': full_record, 'Type': type, 'TTL': 300, 'ResourceRecords':
                 [
@@ -73,12 +85,13 @@ def update_ot(route53, domain, record, ip, version=4):
 def main():
     """Main function."""
     conf, domains = readconfig()
+    """
     #route53 = boto3.client('route53',
                            #aws_access_key_id=conf['access_key'],
                            #aws_secret_access_key=conf['secret'])
     #response = update_ot(route53, 'meodo.com', 'foo', '185.35.77.26')
+    """
     print conf, domains
-
 
 
 if __name__ == '__main__':
