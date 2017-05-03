33a633bf30e082309e679b90c18a65f399500dbc
Peter Eriksson <peter.eriksson@wigetmedia.com>
2017-05-03 09:52:25 +0200

    Fixed flak8

--- scan_for_amt.py
+++ scan_for_amt.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python2
+"""Tool to scan a subnet for AMT."""
+
 import requests
 
-for i in range(1,255):
-    url="https://10.0.111."+str(i)+":16992"
-    print "Testing: %s" % url
-    try:
-        r = requests.get(url, timeout=1)
-    except:
-        continue
 
-    if r.status_code != 200:
-        break
+def main():
+    """Everything in main."""
+    for i in range(1, 255):
+        url = "https://10.0.111." + str(i) + ":16992"
+        print "Testing: %s" % url
+        try:
+            r = requests.get(url, timeout=1)
+        except:
+            continue
 
-    server = str(r.headers['server'])
+        if r.status_code != 200:
+            break
 
-    result = server.find("anagemen")
-    if result != 0:
-        print "Vulnurable: %s %s" % (url, result)
+        server = str(r.headers['server'])
 
-    if server == "AMT":
-        print "Vulnurable: %s %s" % (url, result)
+        result = server.find("anagemen")
+        if result != 0:
+            print "Vulnurable: %s %s" % (url, result)
+
+        if server == "AMT":
+            print "Vulnurable: %s %s" % (url, result)
+
+if __name__ == '__main__':
+    main()
