b3848bb7d800621d233f97206b8c976567b9486c
Peter Eriksson <petereriksson.net@gmail.com>
2018-01-09 10:44:22 +0100

    Linting

--- yaml_syntax_check.py
+++ yaml_syntax_check.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python2
+"""Simple syntax checker for yaml."""
 
 import sys
 import yaml
 
+# FLAGS
+exit_code = 0
+
 
 def main(files):
+    """Looping thru a list of files and tries to load em."""
     for f in files:
         try:
             yaml.safe_load(file(f))
@@ -13,7 +18,10 @@ def main(files):
             continue
         except yaml.parser.ParserError:
             print "File: %s parse errors" % f
+            exit_code = 1
             continue
+    sys.exit(exit_code)
+
 
 if __name__ == '__main__':
     files = sys.stdin.readline()
