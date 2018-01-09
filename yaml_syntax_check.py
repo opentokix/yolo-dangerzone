1d865383c7fb2c36caaa56c394b036edff393fff
Peter Eriksson <petereriksson.net@gmail.com>
2018-01-09 11:18:56 +0100

    Version 1

--- yaml_syntax_check.py
+++ yaml_syntax_check.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python2
 """Simple syntax checker for yaml."""
 
+
 import sys
 import yaml
 
-# FLAGS
-exit_code = 0
+
+def usage():
+    """Usage.
+
+    Use:
+       find /path/to/yaml/files -name "*.yaml" -print0 | xargs -0 -r |yaml_syntax_check.py
+    """
 
 
 def main(files):
     """Looping thru a list of files and tries to load em."""
+    exit_code = 0
+    num_files = 0
     for f in files:
+        num_files += 1
         try:
             yaml.safe_load(file(f))
         except yaml.reader.ReaderError:
             print "File: %s contains errors" % f
+            exit_code = 1
             continue
         except yaml.parser.ParserError:
             print "File: %s parse errors" % f
             exit_code = 1
             continue
+    print "Checked %d files for yaml syntax." % num_files
     sys.exit(exit_code)
 
 
