897c19ad3d5e187adb9d71600a4deb8a730699a2
OpenTokix <petereriksson.net@gmail.com>
2018-01-20 09:58:30 +0100

    Added a more robust authmodule.

--- ovirt/add_vm.py
+++ ovirt/add_vm.py
@@ -7,6 +7,57 @@ import ovirtsdk4.types as types
 import time
 import ConfigParser
 from optparse import OptionParser
+import os
+import sys
+import uuid
+
+
+def construct_credentials(opts):
+    """Construct credentials. First read environment, then command line, lastly ini."""
+    raw_credentials = {'username': '',
+                       'password': '',
+                       'url': ''}
+    try:
+        raw_credentials['username'] = os.environ['ovirt_user']
+    except KeyError:
+        pass
+    try:
+        raw_credentials['password'] = os.environ['ovirt_password']
+    except KeyError:
+        pass
+    try:
+        raw_credentials['url'] = os.environ['ovirt_url']
+    except KeyError:
+        pass
+
+    if opts.password:
+        raw_credentials['password'] = opts.password
+    if opts.username:
+        raw_credentials['username'] = opts.username
+    if opts.url:
+        raw_credentials['url'] = opts.url
+
+    if opts.credentials:
+        cred_ini = opts.credentials
+        config = ConfigParser.ConfigParser()
+        config.read(cred_ini)
+        try:
+            raw_credentials['username'] = config.get('ovirt', 'user')
+        except:
+            pass
+        try:
+            raw_credentials['password'] = config.get('ovirt', 'pass')
+        except:
+            pass
+        try:
+            raw_credentials['url'] = config.get('ovirt', 'url')
+        except:
+            pass
+    for key in raw_credentials:
+        if len(raw_credentials[key]) == 0:
+            print "%s option is missing" % key
+            sys.exit(1)
+    return raw_credentials
 
 
 def add_vm(api, options):
@@ -86,24 +137,21 @@ def add_nic_to_vm(api, options):
 
 def main(opts):
     """Magic main."""
-    options = {}
-    options['credentials'] = opts.credentials
+    options = construct_credentials(opts)
     options['num_cpus'] = int(opts.num_cpus)
     options['ram_amount'] = int(opts.ram_amount)
-    options['vm_name'] = opts.vm_name
+    if opts.vm_name:
+        options['vm_name'] = opts.vm_name
+    else:
+        options['vm_name'] = str(uuid.uuid1())
     options['vm_dc'] = opts.vm_dc
     options['os_type'] = opts.vm_dist
-    config = ConfigParser.ConfigParser()
-    config.read(options['credentials'])
-    options['username'] = config.get('ovirt', 'user')
-    options['password'] = config.get('ovirt', 'pass')
-    options['url'] = config.get('ovirt', 'url')
-    url = options['url']
-    user = options['username']
-    password = options['password']
     options['vcpus'] = types.CpuTopology(cores=options['num_cpus'], sockets=1)
     options['vmem'] = int(options['ram_amount']) * 2**30
-    api = sdk.Connection(url, user, password, insecure=True)
+    api = sdk.Connection(options['url'],
+                         options['username'],
+                         options['password'],
+                         insecure=True)
     add_vm(api, options)
     add_nic_to_vm(api, options)
     add_disk_to_vm(api, options)
@@ -112,12 +160,15 @@ def main(opts):
 
 if __name__ == '__main__':
     p = OptionParser()
-    p.add_option("-n", "--name", dest="vm_name", help="Name of VM", metavar="myvm")
-    p.add_option("-c", "--cpus", dest="num_cpus", help="number of cpus", metavar=1)
-    p.add_option("-r", "--ram", dest="ram_amount", help="Amount of ram in GB", metavar=2)
-    p.add_option("-o", "--osdisk", dest="osdisk", help="Amount of disk in GB", metavar=30)
-    p.add_option("-d", "--datacenter", dest="vm_dc", help="Target datacenter", metavar="hypercube1")
+    p.add_option("-u", "--user", dest="username", help="Username for ovirtmanager", metavar="user@domain", default="admin@internal")
+    p.add_option("-U", "--url", dest="url", help="Url of the ovirt api: https://ovirtmanager.example.com/ovirt-engine/api", metavar="STRING")
+    p.add_option("-p", "--password", dest="password", help="Password for ovirtmanager")
+    p.add_option("-n", "--name", dest="vm_name", help="Name of VM")
+    p.add_option("-c", "--cpus", dest="num_cpus", help="number of cpus", metavar="INT", default=1)
+    p.add_option("-r", "--ram", dest="ram_amount", help="Amount of ram in GB", metavar="INT", default=2)
+    p.add_option("-o", "--osdisk", dest="osdisk", help="Amount of disk in GB", metavar="INT", default=30)
+    p.add_option("-d", "--datacenter", dest="vm_dc", help="Target datacenter", metavar="STRING", default="Hypercube1")
     p.add_option("-z", "--credentials", dest="credentials", help="Credentials location")
-    p.add_option("-l", "--linux_dist", dest="vm_dist", help="Linux Dist (rhel_7x64, rhel_6x64, debian7)", metavar='rhel_7x64')
+    p.add_option("-l", "--linux_dist", dest="vm_dist", help="Linux Dist (rhel_7x64, rhel_6x64, debian7)", metavar="STRING", default='rhel_7x64')
     (opts, args) = p.parse_args()
     main(opts)
---
+++ ovirt/list_vms.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+"""List all running VMs."""
+
+
+import ovirtsdk4 as sdk
+import ConfigParser
+from optparse import OptionParser
+import os
+import sys
+
+
+def lists_vms(api, options):
+    """List all vms."""
+    vms_service = api.system_service().vms_service()
+    vms = vms_service.list()
+    for vm in vms:
+        print("%s: %s" % (vm.name, vm.id))
+
+
+def construct_credentials(opts):
+    """Construct credentials. First read environment, then command line, lastly ini."""
+    raw_credentials = {'username': '',
+                       'password': '',
+                       'url': ''}
+    try:
+        raw_credentials['username'] = os.environ['ovirt_user']
+    except KeyError:
+        pass
+    try:
+        raw_credentials['password'] = os.environ['ovirt_password']
+    except KeyError:
+        pass
+    try:
+        raw_credentials['url'] = os.environ['ovirt_url']
+    except KeyError:
+        pass
+
+    if opts.password:
+        raw_credentials['password'] = opts.password
+    if opts.username:
+        raw_credentials['username'] = opts.username
+    if opts.url:
+        raw_credentials['url'] = opts.url
+
+    if opts.credentials:
+        cred_ini = opts.credentials
+        config = ConfigParser.ConfigParser()
+        config.read(cred_ini)
+        try:
+            raw_credentials['username'] = config.get('ovirt', 'user')
+        except:
+            pass
+        try:
+            raw_credentials['password'] = config.get('ovirt', 'pass')
+        except:
+            pass
+        try:
+            raw_credentials['url'] = config.get('ovirt', 'url')
+        except:
+            pass
+    for key in raw_credentials:
+        if len(raw_credentials[key]) == 0:
+            print "%s option is missing" % key
+            sys.exit(1)
+    return raw_credentials
+
+
+def main(opts):
+    """Magic main."""
+    options = construct_credentials(opts)
+    url = options['url']
+    user = options['username']
+    password = options['password']
+    try:
+        api = sdk.Connection(url, user, password, insecure=True)
+    except sdk.AuthError:
+        print "Authentication error"
+        sys.exit(1)
+    except:
+        print "Unknown connection error"
+        sys.exit(1)
+
+    lists_vms(api, options)
+    api.close()
+
+
+if __name__ == '__main__':
+    p = OptionParser()
+    p.add_option("-u", "--user", dest="username", help="Username for ovirtmanager")
+    p.add_option("-U", "--url", dest="url", help="Url of the ovirt api: https://ovirtmanager.example.com/ovirt-engine/api")
+    p.add_option("-p", "--password", dest="password", help="Password for ovirtmanager")
+    p.add_option("-z", "--credentials", dest="credentials", help="Credentials location")
+    (opts, args) = p.parse_args()
+    main(opts)
