5ea45b1407099263efac29955b24b40a038817e3
OpenTokix <petereriksson.net@gmail.com>
2018-03-27 09:57:16 +0200

    initial commit

--- es_post.sh
+++ es_post.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+es_server=localhost
+es_port=9200
+es_type=_doc
+es_index=info
+
+
+# check for curl and wget
+curl_bin=`whereis -b curl|cut -d " " -f 2`
+wget_bin=`whereis -b wget|cut -d " " -f 2`
+
+# hereis -b returns only "name:" if not found, but if found that will be cut.
+# ugly hack, but it works.
+if [[ $curl_bin != *":"* ]]
+then
+    http_prog=curl
+elif [[ $wget_bin != *":"* ]]
+then
+    http_prog=wget
+else
+    echo "Neither curl or wget found, exiting. This script requires one of them."
+    exit 1
+fi
+
 hostname=`hostname -f|sed 's/\./_/g'`
 tag=$hostname",change"
 username=`whoami`
@@ -12,12 +35,20 @@ else
 fi
 
 if [ -z ${SUDO_USER+x} ]
-then 
+then
     username=`whoami`
-else 
+else
     username=$SUDO_USER
 fi
 
 data="{\"user\": \"${username}\", \"message\": \"${message}\", \"tags\": \"${tag}\"}"
-
-curl -XPUT -i -H 'Content-Type: application/json' -d '\"$data\"' http://localhost:9200/info?pretty
+if [ $http_prog == 'curl' ]
+then
+    $curl_bin -O /dev/null -s -H 'Content-Type: application/json' -d "$data" http://$es_server:$es_port/$es_index/$es_type?pretty
+elif [ $http_prog == 'wget' ]
+then
+    $wget_bin -q -O /dev/null --post-data="$data" --header=Content-Type:application/json "http://$es_server:$es_port/$es_index/$es_type?pretty"
+else
+    echo "Not sure how you came this far, but something is wrong"
+    exit 1
+fi
