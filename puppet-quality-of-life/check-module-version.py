b995e7220c5864bd654d39933cbe0a42fb80a26b
Peter Eriksson <peter@txq.se>
2022-01-28 20:32:43 +0000

    Added support for checking full puppetfile

--- puppet-quality-of-life/check-module-version.py
+++ puppet-quality-of-life/check-module-version.py
@@ -14,30 +14,46 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 def get_module_latest(name, forge):
-    url = f"{forge}/v3/modules/{name}"
-    try:
-        response = urlopen(url)
-    except Exception as e:
-        logger.error(f"{url} did not work with {e}")
-    try:
-        data_json = json.loads(response.read())
-    except Exception as e:
-        logger.error(f"Probelem with json: {e}")
-        exit(1)
-    data = data_json['current_release']['version']
-    return data
+  url = f"{forge}/v3/modules/{name}"
+  try:
+    response = urlopen(url)
+  except Exception as e:
+    logger.error(f"{url} did not work with {e}")
+  try:
+    data_json = json.loads(response.read())
+  except Exception as e:
+    logger.error(f"Probelem with json: {e}")
+    exit(1)
+  data = data_json['current_release']['version']
+  return data
 
+def check_file(puppetfile, forge):
+  with open(puppetfile) as fh:
+    for line in fh:
+      line = fh.readline()
+      if not line.startswith('#'):
+        module = line.split(" ")[1].replace("'", "").replace(",","")
+        version = get_module_latest(module, forge)
+        print(f"mod '{module}', '{version}'")
+          
 @click.command()
-@click.option('--name', '-N', required=True, default=None, help="The module you want version of")
+@click.option('--name', '-N', required=False, default=None, help="The module you want version of")
 @click.option('--forge', '-F', required=False, default="https://forgeapi-cdn.puppet.com", help="Forge url")
 @click.option('--all', '-A', is_flag=True, default=False, help="List all version of the module")
-def main(name, forge, all):
+@click.option('--file', '-f', 'puppetfile', required=False, default=None, help="Print out a complete upgraded Puppetfile")
+def main(name, forge, all, puppetfile):
+  if (name == puppetfile):
+    logger.error("One of name or file is required")
+    exit(1)
+  if puppetfile:
+    check_file(puppetfile, forge)
+  if name:
     if not all:
-        d = get_module_latest(name, forge)
-        print(d)
+      d = get_module_latest(name, forge)
+      print(d)
     else:
-        logger.error("All flag is not implemented yet")
-        exit(1)
+      logger.error("All flag is not implemented yet")
+      exit(1)
 
 if __name__ == '__main__':
     main()
