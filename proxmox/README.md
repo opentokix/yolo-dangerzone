44dcf9f6c2ba4bb3c71b7934921ae887c2f3d8d7
OpenTokix <petereriksson.net@gmail.com>
2022-05-15 11:56:49 +0200

    Adding ceph neuclear option

---
+++ proxmox/README.md
@@ -0,0 +1,5 @@
+# Proxmox scripts 
+
+Scripts in this folder are generally VERY destructive of your proxmox cluster. 
+
+
---
+++ proxmox/ceph-neuclear.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+systemctl stop ceph-mon.target
+systemctl stop ceph-mgr.target
+systemctl stop ceph-mds.target
+systemctl stop ceph-osd.target
+sleep 3
+rm -rf /etc/systemd/system/ceph*
+sleep 3
+killall -9 ceph-mon ceph-mgr ceph-mds
+sleep 3
+rm -rf /var/lib/ceph/mon/  /var/lib/ceph/mgr/  /var/lib/ceph/mds/
+sleep 3
+pveceph purge
