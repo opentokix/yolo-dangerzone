f87e29bdffd856b0d8ce7652414cce4a2fbfbb89
OpenTokix <petereriksson.net@gmail.com>
2022-09-13 10:15:34 +0200

    Added folder logic

--- webscreenshoot_tool.py
+++ webscreenshoot_tool.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+from genericpath import isdir
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timezone
+from os import path, mkdir
 
 def clean_up(browser, display):
     browser.quit()
@@ -18,20 +20,30 @@ def ab(browser, display):
         print("Could not click", e)
         clean_up(browser, display)
         pass
-
-
-
+def folderlogic(folder):
+    if path.isdir(folder):
+        return
+    else:
+        try:
+            mkdir(folder)
+        except:
+            print("Critical: Could not create folder")
+            exit(1)
 
 def main():
-    hostname = "svt.se"
     d = datetime.now(timezone.utc)
-    filename = f"{d.strftime('%Y%m%d.%H%M%S')}.png"
+    day = f"{d.strftime('%Y%m%d')}"
+    tod = f"{d.strftime('%H%M%S')}"
+    hostname = "svt.se"
+    rootfolder = "/data/mounts/archive/webscrape"
+    folderlogic(f"{rootfolder}/{hostname}/{day}")
+    filename = f"{day}.{tod}.png"
     display = Display(visible=0, size=(1080, 3000))
     display.start()
     browser = webdriver.Firefox()
     browser.set_window_size(1080, 3000)
     browser.get(f"https://{hostname}")
-    browser.save_screenshot(f"/data/mounts/archive/webscrape/svt.se/{filename}")
+    browser.save_screenshot(f"{rootfolder}/{hostname}/{day}/{filename}")
     clean_up(browser, display)
 
 if __name__ == '__main__':
