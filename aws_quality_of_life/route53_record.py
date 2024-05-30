4346b730d86951ac7a3ee7f84b2ee975164b85a9
OpenTokix <petereriksson.net@gmail.com>
2024-05-30 22:56:27 +0200

    Adding some type hinting and fixing pep8

--- aws_quality_of_life/route53_record.py
+++ aws_quality_of_life/route53_record.py
@@ -6,6 +6,7 @@ import os
 import logging
 import ipaddress
 
+
 logger = logging.getLogger('route53_tool')
 logger.setLevel(logging.ERROR)
 ch = logging.StreamHandler()
@@ -15,8 +16,11 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-def get_available_zones(route53):
-    """Get zones handled by this account, for some basic checks. Put result in a dict"""
+def get_available_zones(route53) -> dict:
+    """
+    Get zones handled by this account, for some
+    basic checks. Put result in a dict
+    """
     z = {}
     response = route53.list_hosted_zones()
     zones = response['HostedZones']
@@ -29,7 +33,8 @@ def get_available_zones(route53):
         logger.debug(z)
         return z
 
-def sanitycheck_of_zone(zones, fqdn):
+
+def sanitycheck_of_zone(zones, fqdn) -> str:
     """Make sure you own the zone you are trying to add records in."""
     if fqdn.count('.') < 2:
         logger.error("You need a hostname. ie: hostname.domain.tld, not only domain.tld")
@@ -45,12 +50,12 @@ def sanitycheck_of_zone(zones, fqdn):
     exit(1)
 
 
-def add_rrset(route53, zone_id, fqdn, ip):
+def add_rrset(route53, zone_id, fqdn, ip) -> dict:
     """Adding the resource record, also check if its A or AAAA to be added."""
     try:
         address = ipaddress.ip_address(ip)
-    except:
-        logger.error(f"[ {ip} ] is not a valid ipaddress")
+    except ValueError as e:
+        logger.error(f"[ {ip} ] is not a valid ipaddress. ValueError: {e}")
     logger.debug(f"{address} is ip version {address.version}")
     if address.version == 4:
         record_type = "A"
@@ -59,33 +64,39 @@ def add_rrset(route53, zone_id, fqdn, ip):
     else:
         logger.error("Unknown IP")
         exit(1)
-
     response = route53.change_resource_record_sets(
-        HostedZoneId=zone_id, ChangeBatch={'Comment': 'Autoupdated record', 'Changes': [
-        {'Action': 'UPSERT', 'ResourceRecordSet': {
-            'Name': fqdn, 'Type': record_type, 'TTL': 300, 'ResourceRecords':
-            [
-                {'Value': ip},
-            ],
-        }
-        },
-        ]})
+        HostedZoneId=zone_id, ChangeBatch={'Comment': 'Autoupdated record', 'Changes': [  # noqa: E501
+                                          {'Action': 'UPSERT',
+                                           'ResourceRecordSet': {
+                                             'Name': fqdn,
+                                             'Type': record_type,
+                                             'TTL': 300, 'ResourceRecords':
+                                             [
+                                                 {'Value': ip},
+                                               ],
+                                           }
+                                           },
+                                        ]})
     return response
 
+
 @click.command()
-@click.option('--ip', '-i', required=True, default=None, help="The ip-number your A or AAAA record should point to")
-@click.option('--fqdn', required=True, help="Fully qualitifed domain name of the A or AAAA record to be added.")
-@click.option('--verbose', '-V', is_flag=True, default=False, help="Verbose flag to get debug info")
-def main(ip, fqdn, verbose):
+@click.option('--ip', '-i', required=True, default=None, help="The ip-number your A or AAAA record should point to")  # noqa: E501
+@click.option('--fqdn', required=True, help="Fully qualitifed domain name of the A or AAAA record to be added.")  # noqa: E501
+@click.option('--verbose', '-V', is_flag=True, default=False, help="Verbose flag to get debug info")  # noqa: E501
+@click.option('--aws_access', '-A', default=None, required=True, help="AWS Access Key, needs route53 permissions.")  # noqa: E501
+@click.option('--aws_secret', '-S', default=None, required=True, help="AWS Secret Key, needs route53 permissions.")  # noqa: E501
+def main(ip, fqdn, verbose, aws_access, aws_secret):
     """This is where the magic happens."""
     if verbose:
         logger.setLevel(logging.DEBUG)
-    AWS_ACCESS = os.environ['ROUTE53_ACCESS']
-    AWS_SECRET = os.environ['ROUTE53_SECRET']
     logger.debug(fqdn)
-    route53 = boto3.client('route53', aws_access_key_id=AWS_ACCESS, aws_secret_access_key=AWS_SECRET)
+    route53 = boto3.client('route53',
+                           aws_access_key_id=aws_access,
+                           aws_secret_access_key=aws_secret)
     zone_id = sanitycheck_of_zone(get_available_zones(route53), fqdn)
     logger.info(add_rrset(route53, zone_id, fqdn, ip))
 
+
 if __name__ == '__main__':
-    main()
+    main(auto_envvar_prefix='ROUTE53')
